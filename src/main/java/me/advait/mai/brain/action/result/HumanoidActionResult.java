d4b13b32257cba0a41af4e77be9c8fc2220c1efc
advait <git@advait.fun>
2025-01-20 18:02:34 -0500

    Started work on HumanoidActionResultEvent

--- src/main/java/me/advait/mai/brain/action/result/HumanoidActionResult.java
+++ src/main/java/me/advait/mai/brain/action/result/HumanoidActionResult.java
@@ -1,5 +1,10 @@
 package me.advait.mai.brain.action.result;
 
+import me.advait.mai.Mai;
+import me.advait.mai.brain.action.HumanoidAction;
+import me.advait.mai.brain.action.event.HumanoidActionResultEvent;
+import org.bukkit.Bukkit;
+
 public class HumanoidActionResult {
 
     private final boolean success;
@@ -8,6 +13,15 @@ public class HumanoidActionResult {
     public HumanoidActionResult(boolean success, String message) {
         this.success = success;
         this.message = message;
+    }
+
+    public HumanoidActionResult(boolean success, String message, Class<?> resultClassOrigin) {
+        this(success, message);
+        if (!HumanoidAction.class.isAssignableFrom(resultClassOrigin)) {
+            Mai.log().severe("A non-HumanoidAction class was passed into HumanoidActionResult: " + resultClassOrigin);
+            throw new IllegalArgumentException("A non-HumanoidAction class was passed into HumanoidActionResult: " + resultClassOrigin);
+        }
+        // TODO: Call HumanoidActionResultEvent
     }
 
     public boolean isSuccess() {
