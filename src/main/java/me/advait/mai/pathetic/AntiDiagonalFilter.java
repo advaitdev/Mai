bcf4d0e37a04c0c1d5dd7182d4ce776ab4b78f01
advait <git@advait.fun>
2025-01-24 17:01:18 -0500

    AntiDiagonalFilter bug fix x2

--- src/main/java/me/advait/mai/pathetic/AntiDiagonalFilter.java
+++ src/main/java/me/advait/mai/pathetic/AntiDiagonalFilter.java
@@ -5,6 +5,7 @@ import de.metaphoriker.pathetic.api.pathing.filter.PathValidationContext;
 import de.metaphoriker.pathetic.api.provider.NavigationPointProvider;
 import de.metaphoriker.pathetic.api.wrapper.PathPosition;
 import de.metaphoriker.pathetic.bukkit.provider.BukkitNavigationPoint;
+import me.advait.mai.monitor.Monitor;
 import org.jetbrains.annotations.NotNull;
 
 public class AntiDiagonalFilter implements PathFilter {
@@ -23,6 +24,8 @@ public class AntiDiagonalFilter implements PathFilter {
         int parentZ = parentNavigationPoint.getBlockState().getZ();
         int currentX = currentNavigationPoint.getBlockState().getX();
         int currentZ = currentNavigationPoint.getBlockState().getZ();
+
+        Monitor.log("parentX: " + parentX + ", currentX: " + currentX + ", parentZ: " + parentZ + ", currentZ: " + currentZ);
 
         // If x and z are both different, we went diagonally; the path isn't valid.
         return parentX == currentX || parentZ == currentZ;
