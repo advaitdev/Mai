089f58454badb4fbfa6430fce869e4f71758d9d5
advait <git@advait.fun>
2025-01-25 20:25:11 -0500

    Anti diagonal filter bug fix x14 + workflow update

--- src/main/java/me/advait/mai/pathetic/BridgeRealismFilter.java
+++ src/main/java/me/advait/mai/pathetic/BridgeRealismFilter.java
@@ -29,7 +29,7 @@ public class BridgeRealismFilter implements PathFilter {
 
         // If we are bridging "downwards," the only way this is possible is if there's already a solid block placed next to the current location.
         else if (current.getY() < parent.getY()) {
-            return (parent.getX() == current.getX() || parent.getZ() == current.getZ()) && !isSurroundedByAir(current);
+            return (parent.getX() != current.getX() || parent.getZ() != current.getZ()) && !isSurroundedByAir(current);
         }
 
         // If we are bridging "upwards," just check to make sure that our x and z haven't changed. If they have, we're bridging diagonally, which is impossible.
