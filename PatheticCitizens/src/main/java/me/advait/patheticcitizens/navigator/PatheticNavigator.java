091e48dafa1ab3474b748dce95007e7dde07f380
advait <git@advait.fun>
2025-02-25 17:16:34 -0500

    Changed Mai pom

--- PatheticCitizens/src/main/java/me/advait/patheticcitizens/navigator/PatheticNavigator.java
+++ PatheticCitizens/src/main/java/me/advait/patheticcitizens/navigator/PatheticNavigator.java
@@ -2,11 +2,20 @@ package me.advait.patheticcitizens.navigator;
 
 import net.citizensnpcs.api.npc.NPC;
 import net.citizensnpcs.npc.ai.CitizensNavigator;
+import org.bukkit.Location;
 
 public final class PatheticNavigator extends CitizensNavigator {
 
     public PatheticNavigator(NPC npc) {
         super(npc);
+    }
+
+    public void setWalkableTarget(Location target) {
+        // TODO
+    }
+
+    public void setBuildableTarget(Location target) {
+        // TODO
     }
 
 }
