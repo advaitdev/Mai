1546c4ca3588ff770f60c8a5cc43b495c6d54044
advait <git@advait.fun>
2025-03-15 18:59:39 -0400

    Changed PatheticUtil queue code

--- PatheticCitizens/src/main/java/me/advait/patheticcitizens/util/PatheticUtil.java
+++ PatheticCitizens/src/main/java/me/advait/patheticcitizens/util/PatheticUtil.java
@@ -2,46 +2,33 @@ package me.advait.patheticcitizens.util;
 
 import de.metaphoriker.pathetic.api.pathing.result.Path;
 import de.metaphoriker.pathetic.api.pathing.result.PathfinderResult;
-import de.metaphoriker.pathetic.api.wrapper.PathPosition;
 import de.metaphoriker.pathetic.bukkit.mapper.BukkitMapper;
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
-import org.bukkit.util.Vector;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CompletionStage;
+import java.util.concurrent.atomic.AtomicReference;
 
 public final class PatheticUtil {
 
-
-
+    /**
+     * Attempts to convert a Pathetic path into a queue of Bukkit locations.
+     * @param patheticPath The CompletionStage (async) path returned by Pathetic.
+     * @return A queue (FIFO) of the path as locations; null if no path was found.
+     */
     public static @Nullable Queue<Location> toLocationQueue(CompletionStage<PathfinderResult> patheticPath) {
-        try {
-            CompletableFuture<PathfinderResult> future = patheticPath.toCompletableFuture();
-            PathfinderResult result = future.join();  // *Synchronously* waits for the result
-
-            Queue<Location> locationQueue = new LinkedList<>();
-
+        AtomicReference<Queue<Location>> locations = new AtomicReference<>(new LinkedList<>());
+        patheticPath.thenAccept(result -> {
             if (result.successful()) {
                 Path path = result.getPath();
-                for (PathPosition pathPosition : path) {
-                    Vector vector = BukkitMapper.toVector(pathPosition.toVector());
-                    Location location = new Location(
-                            Bukkit.getWorld(pathPosition.getPathEnvironment().getName()),
-                            vector.getX(), vector.getY(), vector.getZ()
-                    );
-                    locationQueue.offer(location);
-                }
+                path.forEach(pathPosition -> locations.get().offer(BukkitMapper.toLocation(pathPosition)));
+            } else {
+                locations.set(null);
             }
-
-            return locationQueue;
-
-        } catch (Exception e) {
-            return null;
-        }
+        });
+        return locations.get();
     }
 
 }
