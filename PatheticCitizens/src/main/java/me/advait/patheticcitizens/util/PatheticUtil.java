68aabad57128a5cde6633e8fe94eb46dd163fd96
advait <git@advait.fun>
2025-03-16 15:02:06 -0400

    Changing setWalkableTarget x3

--- PatheticCitizens/src/main/java/me/advait/patheticcitizens/util/PatheticUtil.java
+++ PatheticCitizens/src/main/java/me/advait/patheticcitizens/util/PatheticUtil.java
@@ -76,7 +76,7 @@ public final class PatheticUtil {
 
     public static void sendDebugPath(Player player, Location location) {
         Particle.DustOptions dustOptions = new Particle.DustOptions(Color.fromRGB(0, 127, 255), 1.0F);
-        player.spawnParticle(Particle.HEART, location, 50, dustOptions);
+        player.spawnParticle(Particle.DUST_COLOR_TRANSITION, location, 50, dustOptions);
     }
 
 }
