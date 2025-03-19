29ccbc0f565ab73f77326bb7dd6e33ac8c983bfe
advait <git@advait.fun>
2025-03-18 22:21:19 -0400

    dude i'm just using citizens's navigator i can't do this anymore

--- PatheticCitizens/src/main/java/me/advait/patheticcitizens/navigator/PatheticNavigator.java
+++ PatheticCitizens/src/main/java/me/advait/patheticcitizens/navigator/PatheticNavigator.java
@@ -1,16 +1,26 @@
 package me.advait.patheticcitizens.navigator;
 
+import de.metaphoriker.pathetic.bukkit.mapper.BukkitMapper;
 import me.advait.patheticcitizens.PatheticCitizens;
 import me.advait.patheticcitizens.npc.PatheticNPC;
 import me.advait.patheticcitizens.pathfinder.PatheticAgent;
+import me.advait.patheticcitizens.util.PatheticUtil;
 import net.citizensnpcs.api.ai.NavigatorParameters;
+import net.citizensnpcs.api.astar.pathfinder.Path;
+import net.citizensnpcs.api.npc.NPC;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.scheduler.BukkitScheduler;
+import org.bukkit.util.Vector;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class PatheticNavigator {
 
     private final PatheticNPC npc;
+
+    private final PatheticAgent AGENT = PatheticAgent.getInstance();
     private final BukkitScheduler scheduler = Bukkit.getScheduler();
 
     public PatheticNavigator(PatheticNPC npc) {
@@ -18,13 +28,27 @@ public class PatheticNavigator {
     }
 
     public void setWalkableTarget(Location target) {
-        PatheticNavigationStrategy patheticNavigationStrategy = new PatheticNavigationStrategy(npc.getCitizensNPC(), target, new NavigatorParameters());
-        scheduler.runTaskTimer(PatheticCitizens.getInstance(), task -> {
-            if (patheticNavigationStrategy.isComplete()) {
-                patheticNavigationStrategy.stop();
-                task.cancel();
-            } else patheticNavigationStrategy.update();
-        }, 0, 1);
+        NPC citizensNPC = npc.getCitizensNPC();
+        AGENT.getGroundPath(citizensNPC.getStoredLocation(), target).thenAccept(result -> {
+            if (result.successful()) {
+                Bukkit.getScheduler().runTask(PatheticCitizens.getInstance(), () -> {
+                    List<Vector> pathVectors = new ArrayList<>();
+                    result.getPath().forEach(pathPosition -> {
+                        pathVectors.add(BukkitMapper.toVector(pathPosition.toVector()));
+                    });
+                    citizensNPC.getNavigator().setTarget(pathVectors);
+                });
+            }
+        });
+
+//        PatheticNavigationStrategy patheticNavigationStrategy = new PatheticNavigationStrategy(npc.getCitizensNPC(), target, new NavigatorParameters());
+//        scheduler.runTaskTimer(PatheticCitizens.getInstance(), task -> {
+//            if (patheticNavigationStrategy.isComplete()) {
+//                patheticNavigationStrategy.stop();
+//                task.cancel();
+//            } else patheticNavigationStrategy.update();
+//        }, 0, 1);
+
     }
 
 }
